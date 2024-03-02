3c1fbcf299957d2690e7ece25b651abdeb2aa9b3
frtook <frtook@hotmail.com>
2024-03-02 11:23:34 +0600

    fixed page price

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 let btnPrice = document.querySelector("#price");
+let btnMain = document.querySelector("#main");
+let table = document.querySelector(".table");
+let form = document.querySelector("form");
+let price = document.querySelector(".price");
+// var
 let data = new Date();
 let arDate = new Intl.DateTimeFormat("ar", {
   weekday: "long",
@@ -9,6 +14,35 @@ let arDate = new Intl.DateTimeFormat("ar", {
 
 document.querySelector(".data").textContent = arDate;
 
-btnPrice.onclick = () => {
-  location.pathname = "Net/pages/price.html";
+btnMain.onclick = () => {
+  btnMain.classList.add("active");
+  btnPrice.classList.remove("active");
+  form.style.display = "flex";
+  price.style.display = "none";
 };
+btnPrice.onclick = () => {
+  btnPrice.classList.add("active");
+  btnMain.classList.remove("active");
+  price.style.display = "block";
+  form.style.display = "none";
+};
+
+function createTable() {
+  fetch("../data/price.json")
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((e) => {
+        let { num, time, cost, size } = e;
+        let template = `
+        <div>
+          <span>${num}</span>
+          <span>${time}</span>
+          <span>${cost}</span>
+          <span>${size}</span>
+        </div>
+        `;
+        table.insertAdjacentHTML("beforeend", template);
+      });
+    });
+}
+createTable();
